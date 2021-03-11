92a96cefd54bb64ad0443037fdc3f107ae61965f
sankara-gomathi <71717837+sankara-gomathi@users.noreply.github.com>
2021-03-11 11:25:14 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/app1/ACHROMATIC_TYPES.java
@@ -0,0 +1,13 @@
+package com.example.app1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ACHROMATIC_TYPES extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_c_h_r_o_m_a_t_i_c__t_y_p_e_s);
+    }
+}
