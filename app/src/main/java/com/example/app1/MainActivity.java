8307994ca27cc5ba27721a1ca60929e813e2e886
anantha-natarajan <natarajannandhu@gmail.com>
2021-03-12 18:07:12 -0600

    Test Commit

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.app1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+/**
+    Testing Correct working of GIT
+
+ */
 
 public class MainActivity extends AppCompatActivity {
 
